9fd342b8da8e8485f92cc78d951105227b2c736a
kshitija06 <kshitijahingankar@gmail.com>
2024-05-16 14:34:23 +0530

    Adding Variables details and syntax

---
+++ Varaibles.tf
@@ -0,0 +1,7 @@
+#Variables allow you to define values that can be reused throughout your configuration and can be set via the command line, environment variables, or terraform.tfvars files.
+
+variable "instance_type"{
+    description= "Type of the instance"
+    typr= string
+    default= "t2.micro"
+}
